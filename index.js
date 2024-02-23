8d72d2fe8d222d9c57f3207dbe91b8c4cec4e532
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-24 00:15:24 +0300

    Assignment Done

---
+++ index.js
@@ -0,0 +1,15 @@
+// 1 & 2) Divide function expression
+const divide = function() {
+    return 2000 / 100;
+};
+
+// 3 & 4) Square arrow function
+const square = (x) => x * x;
+
+// 5 & 6) Add arrow function
+const add = (a, b) => a + b;
+
+// Example usage
+console.log(divide()); // 20
+console.log(square(5)); // 25
+console.log(add(10, 20)); // 30
